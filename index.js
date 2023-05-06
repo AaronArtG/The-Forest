e96c10156006a2202a0aba918d7cec01b0079a56
Aaron garcia <aarongarcia@Aarons-MacBook-Pro.local>
2023-05-06 10:45:16 -0700

    player is on the screen

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+class theHeroKinda {
+    constructor() {
+        this.position = {
+          x: 200,
+          y: 200  
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src ="./img/theHero.gif"
+
+        this.image = image 
+        this.width = 100
+        this.height = 100
+
+    }
+
+    draw() {
+        // c.fillStyle = "red"
+        // c.fillRect(this.position.x,this.position.y, this.width, this.height)
+c.drawImage(this.image,this.position.x, this.position.y)
+    }
+}
+const player = new theHeroKinda()
+player.draw()
+
+function animate(){
+    requestAnimationFrame(animate)
+    player.draw()
+}
+
+animate()
