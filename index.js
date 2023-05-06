eda9106d75fdda6429e4fe0ba889684bbed94521
Aaron garcia <aarongarcia@Aarons-MacBook-Pro.local>
2023-05-06 11:39:46 -0700

    guy moves

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ canvas.height = innerHeight
 
 class theHeroKinda {
     constructor() {
-        this.position = {
-          x: 200,
-          y: 200  
-        }
+
         this.velocity = {
             x: 0,
             y: 0
@@ -17,25 +14,102 @@ class theHeroKinda {
 
         const image = new Image()
         image.src ="./img/theHero.gif"
+        image.onload = () => {
+            const scale = 0.50
+            this.image = image 
+            this.width = image.width * scale
+            this.height = image.height* scale
+                   this.position = {
+          x: canvas.width / 2 - this.width / 2,
+          y: canvas.height - this.height - 20
+        }   
+        }
+ }
 
-        this.image = image 
-        this.width = 100
-        this.height = 100
-
-    }
-
-    draw() {
+draw() {
         // c.fillStyle = "red"
         // c.fillRect(this.position.x,this.position.y, this.width, this.height)
-c.drawImage(this.image,this.position.x, this.position.y)
+
+     c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width, 
+        this.height
+        )
+    }
+    update(){
+        if (this.image) {
+        this.draw()
+        this.position.x += this.velocity.x   
+        }
     }
 }
 const player = new theHeroKinda()
-player.draw()
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    },
+}
+
 
 function animate(){
     requestAnimationFrame(animate)
-    player.draw()
+    c.fillStyle = "#E6E6E6"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+
+    if (keys.a.pressed) {
+     player.velocity.x = -5  
+} else if (keys.d.pressed){
+    player.velocity.x = 5
+} else {
+        player.velocity.x = 0
+    }
 }
 
 animate()
+
+addEventListener('keydown',({key}) => {
+    console.log(key)
+    switch(key){
+        case'a':
+        console.log('left')
+    
+        keys.a.pressed = true
+        break
+        case'd':
+        console.log('right')
+        keys.d.pressed = true
+        break
+        case' ':
+        console.log('space')
+        break
+
+    }
+})
+
+addEventListener('keyup',({key}) => {
+    console.log(key)
+    switch(key){
+        case'a':
+        console.log('left')
+    
+        keys.a.pressed = false
+        break
+        case'd':
+        console.log('right')
+        keys.d.pressed = false
+        break
+        case' ':
+        console.log('space')
+        break
+
+    }
+})
