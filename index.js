376fddf7e2f6a0dbe6069aa774051529e93fb0b3
Aaron garcia <aarongarcia@Aarons-MacBook-Pro.local>
2023-05-10 21:19:41 -0700

    lose condition added, however I also lost the abilty to hit one enemy instead of all in a grid?

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ class theHeroKinda {
             y: 0
         }
 this.rotation = 0 
+this.opacity = 1
         const image = new Image()
         image.src ="./img/theHero.gif"
         image.onload = () => {
@@ -30,6 +31,7 @@ draw() {
         // c.fillStyle = "red"
         // c.fillRect(this.position.x,this.position.y, this.width, this.height)
     c.save()
+    c.globalAlpha = this.opacity
     c.translate(
         player.position.x + player.width / 2, 
         player.position.y + player.height / 2 )
@@ -74,6 +76,59 @@ class Projectile {
         c.fillStyle = "black"
         c.fill()
         c.closePath()
+
+    }
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+class Particle {
+    constructor({position, velocity, radius, color}){
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = radius
+        this .color = color
+        this.opacity = 1 
+    }
+
+
+    draw() {
+        c.save()
+        c.globalAlpha = this.opacity
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = this.color
+        c.fill()
+        c.closePath()
+        c.restore()
+
+    }
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        this.opacity -= 0.01
+    }
+}
+
+class InvaderProjectile {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+
+     this.width = 3
+     this.height = 10
+    }
+
+
+    draw() {
+        c.fillStyle = "orange"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
 
     }
     update(){
@@ -128,6 +183,20 @@ draw() {
         this.position.y += velocity.y   
         }
     }
+
+    shoot(invaderProjectiles){
+        invaderProjectiles.push(new InvaderProjectile({
+            position:{
+                x: this.position.x+ this.width / 2,
+                y: this.position.y + this.height
+            },
+            velocity: {
+                x: 0, 
+                y:5
+            }
+        }))
+
+    }
 }
 
 
@@ -180,6 +249,8 @@ class Grid {
 const player = new theHeroKinda()
 const projectiles = []
 const grids = []
+const invaderProjectiles = []
+const particles = []
 
 const keys = {
     a: {
@@ -199,14 +270,79 @@ const keys = {
 
 let frames = 0 
 let randomInterval = Math.floor(Math.random() * 500 + 500)
-console.log(randomInterval)
+let game = {
+    over: false,
+    active: true
+}
+
+
+function createParticles({object, color}){
+    for(let i = 0; i < 15; i++){
+        particles.push(new Particle({
+            position: {
+                x: object.position.x + object.width / 2,
+                y:object.position.y + object.height / 2
+            },
+            velocity:{
+                x: (Math.random()-0.5) *2 ,
+                y:(Math.random()- 0.5) * 2
+            },
+            radius: Math.random()* 3,
+            color: color || "green"
+        }))}  
+}
 
 function animate(){
+    if(!game.active) return
     requestAnimationFrame(animate)
     c.fillStyle = "#E6E6E6"
     c.fillRect(0, 0, canvas.width, canvas.height)
-    
-    player.update()
+     player.update()
+     particles.forEach((particle, i) =>{
+        if(particle.opacity <= 0){
+            setTimeout(() => {
+            particles.splice(i, 1)
+           },0) 
+           } else {
+              particle.update()
+           } 
+      
+      
+     })
+     invaderProjectiles.forEach((invaderProjectile,  index) => {
+        if(invaderProjectile.position.y + invaderProjectile.height >= canvas.height){
+            setTimeout(() =>{ 
+                invaderProjectiles.splice(index, 1)    
+               }, 0)
+        } else invaderProjectile.update()
+// projectile hit player
+        if (invaderProjectile.position.y + invaderProjectile.height 
+            >= 
+            player.position.y && 
+            invaderProjectile.position.x + invaderProjectile.width 
+            >= 
+            player.position.x && 
+            invaderProjectile.position.x <= player.position.x + 
+            player.width
+            ){
+                setTimeout(() =>{ 
+                    invaderProjectiles.splice(index, 1)   
+                    player.opacity = 0 
+                    game.over = true
+                   }, 0)
+
+
+                   setTimeout(() =>{ 
+                    game.active = false
+                   }, 2000)
+            createParticles({
+                object: player,
+                color: "red"
+            })
+        }
+    })
+  
+
     projectiles.forEach((projectile, index) => {
 
         if (projectile.position.y + projectile.radius <= 0) {
@@ -217,12 +353,20 @@ function animate(){
         } else {
         projectile.update()
         }
+
     })
 
     grids.forEach((grid, gridIndex) => {
       grid.update()  
+          
+
+    if(frames % 100 === 0 && grid.invaders.length > 0){
+        grid.invaders[Math.floor(Math.random() * grid.invaders.length)].shoot(invaderProjectiles)
+    }
+
       grid.invaders.forEach((invader, i) => {
         invader.update({velocity: grid.velocity})
+        // projectiles hit enemy
 
         projectiles.forEach((projectile,j) => {
             if(
@@ -232,9 +376,8 @@ function animate(){
                 invader.position.x && projectile.position.x - 
                 projectile.radius <= invader.position.x + invader.width &&
                 projectile.position.y + projectile.radius >= invader.position.y
-
-
-                ){
+            ){
+  
             
                 setTimeout(()=>{
                     const invaderFound = grid.invaders.find(
@@ -244,7 +387,10 @@ function animate(){
                         (projectile2) => projectile2 === projectile
                     )
                     // remove invader and projectile
-                    if (invaderFound && projectileFound){    
+                    if (invaderFound && projectileFound){  
+                        createParticles({
+                            object: invader,
+                        })
                    grid.invaders.splice(i, 1) 
                    projectiles.splice(j, 1)
 
@@ -283,14 +429,18 @@ function animate(){
         grids.push(new Grid())
         randomInterval = Math.floor(Math.random() * 500 + 500)
         frames = 0
-        console.log(randomInterval)
+        //console.log(randomInterval)
     }
+
+
+
     frames++
 }
 
 animate()
 
 addEventListener('keydown',({key}) => {
+    if (game.over) return
     // console.log(key)
     switch(key){
         case'a':
